2d979e6b835f6b4121e68dbc11674549442c0bd3
hposecarames <menuven@trapallada>
2018-10-10 12:09:47 +0200

    Boletin 2 ejercicio 4

---
+++ Boletin2_4/src/boletin2_4/Boletin2_4.java
@@ -0,0 +1,27 @@
+
+package boletin2_4;
+
+import javax.swing.JOptionPane;
+
+public class Boletin2_4 {
+
+    
+    public static void main(String[] args) {
+    
+        int cantidad;       
+        int mon; //Monedas de 1
+        int resto1, resto2;
+              
+        cantidad = Integer.parseInt(JOptionPane.showInputDialog("Introduce la cantidad: "));
+        
+        resto1 = cantidad % 100;        
+        resto2 = resto1 % 20;        
+        mon = resto2 % 5;
+        
+        JOptionPane.showMessageDialog(null,cantidad+" euros son: "+"\n"+(cantidad / 100)+" billetes de 100"+"\n"+(resto1 / 20)+" billetes de 20"+"\n"+(resto2 / 5)+" billetes de 5"+"\n"+mon+" monedas de 1");
+        
+        
+        
+    }
+    
+}
